775f9094a71ab0462e6b73d25e2c5ad9af761c59
Jairo Vanegas <jairo.vanegas@javeriana.edu.co>
2022-05-29 02:16:23 -0500

    Commit pipeline

--- src/main/java/co/edu/javeriana/seshat/sofiplus/SofiplusApplication.java
+++ src/main/java/co/edu/javeriana/seshat/sofiplus/SofiplusApplication.java
@@ -1,24 +1,12 @@
 package co.edu.javeriana.seshat.sofiplus;
 
-import co.edu.javeriana.seshat.sofiplus.Modules.BusinessModule;
-import co.edu.javeriana.seshat.sofiplus.Modules.ModuleMethod;
-import org.reflections.Reflections;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.Objects;
-import java.util.Set;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -32,6 +20,10 @@ public class SofiplusApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("*");
+            }
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//                registry.addResourceHandler("/**").addResourceLocations("file:/home/ubuntu/sofi-frontend/dist/spa");
             }
         };
     }
